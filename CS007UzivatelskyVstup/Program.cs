ff5704c91cf7259fba6432c51afad9ebc0d690ca
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:12:14 +0200

    Add files via upload

---
+++ CS007UzivatelskyVstup/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS007UzivatelskyVstup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Zadejte textový řetězec: ");
+            //Console.WriteLine("Zadali jste: ", Console.ReadLine());
+
+            Console.WriteLine("Postupně zadejte dvě celá čísla: ");
+            Console.WriteLine("Součet čísek je: {0}", Console.ReadLine() + Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Stiskněte libovolnou klávesu.");
+            Console.ReadKey(false);
+        }
+    }
+}
